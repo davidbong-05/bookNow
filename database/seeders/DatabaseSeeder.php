64a3fb9d5c5fe343ae086847bfe475c5decb0340
davidbong-05 <davidbong05@gmail.com>
2022-06-15 08:19:50 +0800

    chores: added facility and court seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Courts;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,96 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('facilities')->insert(
+            [
+                [
+                    'name'          => 'Badminton',
+                    'image'         => '/img/facility/court_1_piapqn.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+                ],
+                [
+                    'name'          => 'Squash',
+                    'image'         => '/img/facility/court_2_rsnujz.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ],
+                [
+                    'name'          => 'Futsal',
+                    'image'         => '/img/facility/court_3_yaov2c.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ],
+                [
+                    'name'          => 'Tennis',
+                    'image'         => '/img/facility/court_4_uqclwm.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ],
+                [
+                    'name'          => 'Netball',
+                    'image'         => '/img/facility/court_5_lh8qlm.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ],
+                [
+                    'name'          => 'Volleyball',
+                    'image'         => '/img/facility/court_6_flzju0.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ],
+                [
+                    'name'          => 'Basketball',
+                    'image'         => '/img/facility/court_7_ua7twy.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ],
+                [
+                    'name'          => 'Takraw',
+                    'image'         => '/img/facility/court_8_glsoyy.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ],
+                [
+                    'name'          => 'Rugby',
+                    'image'         => '/img/facility/court_9_hbt8wl.jpg',
+                    'description'   => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. '
+
+                ]
+            ]
+        );
+
+        Courts::create([
+            'facilities_id' =>  1,
+            'name'          =>  "Badminton 1",
+        ]);
+        Courts::create([
+            'facilities_id' =>  1,
+            'name'          =>  "Badminton 2",
+        ]);
+        Courts::create([
+            'facilities_id' =>  1,
+            'name'          =>  "Badminton 3",
+        ]);
+        Courts::create([
+            'facilities_id' =>  2,
+            'name'          =>  "Squash 1",
+        ]);
+        Courts::create([
+            'facilities_id' =>  2,
+            'name'          =>  "Squash 2",
+        ]);
+        Courts::create([
+            'facilities_id' =>  2,
+            'name'          =>  "Squash 3",
+        ]);
+        Courts::create([
+            'facilities_id' =>  3,
+            'name'          =>  "Futsal A",
+        ]);
+        Courts::create([
+            'facilities_id' =>  3,
+            'name'          =>  "Futsal B",
+        ]);
     }
-}
+};
