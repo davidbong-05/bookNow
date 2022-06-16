67e075a4455c69b691eed449a2d4ee668068ca86
davidbxs <davidbxs@sains.com.my>
2022-06-16 09:36:31 +0800

    update: user-booking relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -45,4 +47,39 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function freshBooking()
+    {
+        return Bookings::where('user_id', $this->id)
+            ->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+            ->where(function (Builder $query) {
+                return $query->where('status', 'Approved')
+                    ->orWhere('status', 'Pending');
+            })
+            ->get();
+    }
+
+    public function bookHistory()
+    {
+        return Bookings::where('user_id', $this->id)
+            ->latest()
+            ->paginate(10);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Bookings::class);
+    }
+
+    public function path($append = '')
+    {
+        $path = route('profile.show', $this->id);
+
+        return $path ? "{$path}/{$append}" : $path;
+    }
 }
