a6fb1849590c3079815a2a238bc98555cbb284f8
davidbxs <davidbxs@sains.com.my>
2022-06-22 10:21:13 +0800

    fix: failed to resolve component on Heorku

--- resources/views/Bookings/show.blade.php
+++ resources/views/Bookings/show.blade.php
@@ -2,7 +2,7 @@
     <div class="container px-4 py-4">
         @forelse ($bookings as $booking)
             <booking-card :booking="{{ $booking }}" :facility='"{{ $booking->courts->facilities->name }}"'
-                :court='"{{ $booking->courts->name }}"' />
+                :court='"{{ $booking->courts->name }}"'> </booking-card>
         @empty
             <p class="text-gray-500 p-4">No booking yet...</p>
         @endforelse
